0f30ee18cc14bc5ce0913b6ce60ce7956c773af7
tadakazu1972 <tadakazu1972@gmail.com>
2017-08-18 21:17:47 +0900

    fix 確認中

--- event.js
+++ event.js
@@ -106,7 +106,7 @@ function postTwitter(){
 
 function postLine(){
   //LINE
-  location.href = "http://line.me/R/msg/text/?https://osakasports.azurewebsites.net/event.html?id="+id + "【大阪市オータムチャレンジスポーツ2017】" +eventData[id].name + "/" + eventData[id].date + "/" + eventData[id].time;
+  location.href = "http://line.me/R/msg/text/?【イベント名】" +eventData[id].name + "%0A【場所】" + eventData[id].facility + "¥n【内容】" + eventData[id].top + "¥n【参加費】" + eventData[id].fee;
 }
 
 function postFacebook(){
